90a3259b7b059f0456a1ce27fb4b6df2f1c8876a
Debsishu Pal <pauldebsishu2002@gmail.com>
2022-04-28 21:52:09 +0530

    Search Bar Reponsive Add

--- src/App.js
+++ src/App.js
@@ -1,35 +1,34 @@
 import React, { useState } from "react";
-import styled from "styled-components"
-import pic from './pagename.png'
-
+import styled from "styled-components";
+import pic from "./pagename.png";
 
 const api = {
   key: "f36453a795e68c34a900630994b84339",
   base: "https://api.openweathermap.org/data/2.5/",
 };
 const Button = styled.button`
-background-color: blue;
-width:100px;
-height:43px;
-color: white;
-padding: 5px 15px;
-border-radius: 10px;
-outline: 0;
-text-transform: uppercase;
-margin: 10px 0px;
-cursor: pointer;
-transition: ease background-color 1000ms;
-&:hover {
-  background-color: black;
-}
-&:disabled {
-  cursor: default;
-  opacity: 0.7;
-}
+  background-color: blue;
+  width: 100px;
+  height: 43px;
+  color: white;
+  padding: 5px 15px;
+  border-radius: 10px;
+  outline: 0;
+  text-transform: uppercase;
+  margin: 10px 0px;
+  cursor: pointer;
+  transition: ease background-color 1000ms;
+  &:hover {
+    background-color: black;
+  }
+  &:disabled {
+    cursor: default;
+    opacity: 0.7;
+  }
 `;
 function App() {
   const [query, setQuery] = useState(""); //Hook
-  const [weather, setWeather] = useState({}); 
+  const [weather, setWeather] = useState({});
 
   const search = (evt) => {
     if (evt.key === "Enter") {
@@ -44,13 +43,13 @@ function App() {
   };
   const click = () => {
     fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-    .then((res) => res.json())
-    .then((result) => {
-      setWeather(result);
-      setQuery("");
-      console.log(result);
-    });
-  }
+      .then((res) => res.json())
+      .then((result) => {
+        setWeather(result);
+        setQuery("");
+        console.log(result);
+      });
+  };
   const dateBuilder = (d) => {
     let months = [
       "January",
@@ -102,7 +101,9 @@ function App() {
             />
           </div>
           <div className="button">
-            <Button className="search" onClick={click}>Search</Button>
+            <Button className="search" onClick={click}>
+              Search
+            </Button>
           </div>
         </div>
         {typeof weather.main != "undefined" ? (

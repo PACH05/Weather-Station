03fc345f8aa225bb8133be7ff920ebefe5914d2b
Prasanta Acharyya <acharyya.prasanta05@gmail.com>
2023-07-02 19:57:30 +0530

    Minor Bug Fix

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
 
   const search = (evt) => {
     if (evt.key === "Enter") {
-      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      fetch(`${api.base}weather?q=${query.trim()}&units=metric&APPID=${api.key}`)
         .then((res) => res.json())
         .then((result) => {
           setWeather(result);
@@ -41,7 +41,7 @@ function App() {
     }
   };
   const click = () => {
-    fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    fetch(`${api.base}weather?q=${query.trim()}&units=metric&APPID=${api.key}`)
       .then((res) => res.json())
       .then((result) => {
         setWeather(result);
